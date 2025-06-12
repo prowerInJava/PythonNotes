7f76442e59cb020b1e3697438a8167a569cddd1f
yinjiahuan <jackhuan@icloud.com>
2025-06-13 03:37:03 +0800

    rm .idea folder

--- python_glob.py
+++ python_glob.py
@@ -1,5 +1,9 @@
 import glob
 
-txt_files = glob.glob('PythonNotes/**/*.py')  # 匹配PythonNotes 文件夹下的多个文件夹下的.py文件
+txt_files = glob.glob('./**/*.py')  # 匹配当前文件夹下的一层文件夹下的.py文件
 for file_path in txt_files:
     print(file_path)
+
+print(glob.glob("*/*/apple.txt"))  # 通配符*，表示匹配所有字符
+print(glob.glob('*/?anana.txt'))  # 通配符？，表示任意一个字符
+
